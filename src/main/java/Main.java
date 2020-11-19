96037bbc60384bc95d40f18198d1d15c17db61c4
RJM <rmurphy1998@gmail.com>
2020-11-19 15:54:30 +0000

    Added printCourses and printStudents methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,41 @@
 import org.joda.time.DateTime;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
+
+    private static ArrayList<CourseProgramme> courses = new ArrayList();
+    private static ArrayList<Student> students = new ArrayList();
+
+    public static void printCourses() {
+        for(CourseProgramme c : courses) {
+            System.out.println(c.getName());
+            for(Module m : c.getModules()) {
+                System.out.println("\t"+m.getName());
+                for(Student s : m.getStudents()) {
+                    System.out.println("\t\t"+s.getName()+" ["+s.getUsername()+"]");
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public static void printStudents() {
+        for(Student s : students) {
+            System.out.println("Name: "+s.getName());
+            System.out.println("Username: "+s.getUsername());
+            System.out.println("Modules:");
+            for(Module m : s.getModules()) {
+                System.out.println("\t"+m.getName());
+            }
+            System.out.println("Course(s):");
+            for(CourseProgramme c : s.getCourses()) {
+                System.out.println("\t"+c.getName());
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String args[]) {
         //Create Students
@@ -19,6 +52,19 @@ public class Main {
         Student s11 = new Student("Shirley", new DateTime("1975-09-13"), 18495837);
         Student s12 = new Student("Annie", new DateTime("1995-09-29"), 18578274);
 
+        students.add(s1);
+        students.add(s2);
+        students.add(s3);
+        students.add(s4);
+        students.add(s5);
+        students.add(s6);
+        students.add(s7);
+        students.add(s8);
+        students.add(s9);
+        students.add(s10);
+        students.add(s11);
+        students.add(s12);
+
         //Create Modules
         Module m1 = new Module("Software Engineering");
         Module m2 = new Module("Machine Learning");
@@ -31,24 +77,33 @@ public class Main {
         CourseProgramme c1 = new CourseProgramme("GY350", new DateTime("2020-09-01"), new DateTime("2021-06-01"));
         CourseProgramme c2 = new CourseProgramme("Greendale", new DateTime("2020-09-01"), new DateTime("2021-06-01"));
 
+        courses.add(c1);
+        courses.add(c2);
+
         //Add Students to Modules
-        for (Student student : Arrays.asList(s1, s2, s3, s4, s5)) {
-            m1.addStudent(student);
+        for (Student s : Arrays.asList(s1, s2, s3, s4, s5)) {
+            m1.addStudent(s);
+            c1.addStudent(s);
         }
-        for (Student student : Arrays.asList(s1, s2, s3, s4, s5)) {
-            m2.addStudent(student);
+        for (Student s : Arrays.asList(s1, s2, s3, s4, s5)) {
+            m2.addStudent(s);
+            c1.addStudent(s);
         }
-        for (Student student : Arrays.asList(s1, s2, s3, s4, s5)) {
-            m3.addStudent(student);
+        for (Student s : Arrays.asList(s1, s2, s3, s4, s5)) {
+            m3.addStudent(s);
+            c1.addStudent(s);
         }
-        for (Student student : Arrays.asList(s6, s7, s8, s9, s10, s11, s12)) {
-            m4.addStudent(student);
+        for (Student s : Arrays.asList(s6, s7, s8, s9, s10, s11, s12)) {
+            m4.addStudent(s);
+            c2.addStudent(s);
         }
-        for (Student student : Arrays.asList(s6, s7, s8, s9, s10, s11, s12)) {
-            m5.addStudent(student);
+        for (Student s : Arrays.asList(s6, s7, s8, s9, s10, s11, s12)) {
+            m5.addStudent(s);
+            c2.addStudent(s);
         }
-        for (Student student : Arrays.asList(s6, s7, s8, s9, s10, s11, s12)) {
-            m6.addStudent(student);
+        for (Student s : Arrays.asList(s6, s7, s8, s9, s10, s11, s12)) {
+            m6.addStudent(s);
+            c2.addStudent(s);
         }
 
         //Add Modules to Course Programme
@@ -58,5 +113,8 @@ public class Main {
         for (Module module : Arrays.asList(m4, m5, m6)) {
             c2.addModule(module);
         }
+
+        printCourses();
+        printStudents();
     }
 }
